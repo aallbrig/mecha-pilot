96076061e2ba43d5870edb58d44e7a8d7d133f96
aallbrig <aallbrig@gmail.com>
2022-10-30 00:52:50 +0000

    Deploying to github-pages from @ aallbrig/mecha-pilot@d9421afbfa40165d6f459663c44d3679f9c39479 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "Mecha Pilot",
-    productVersion: "0.0.2",
+    productVersion: "0.0.3",
     devicePixelRatio: 1,
 }
 
