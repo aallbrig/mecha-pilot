c13e81529f9f2fa85962ae2e2ed21b77dcd09764
Andrew Allbright <aallbrig@gmail.com>
2022-10-31 16:59:40 -0400

    Basic enemies move dumbly towards player

--- unity/mecha-pilot/Assets/Scripts/OpposingForce/HostileAi.cs
+++ unity/mecha-pilot/Assets/Scripts/OpposingForce/HostileAi.cs
@@ -5,6 +5,9 @@ namespace OpposingForce
 {
     public class HostileAi : MonoBehaviour
     {
+        public float detectionDistance = 20f;
+        public float closeEnoughDistance = 5f;
+        public float speed = 3.0f;
         private GameObject _player;
         private Transform _transform;
         private void Start()
@@ -14,6 +17,12 @@ namespace OpposingForce
         }
         private void Update()
         {
+            var distanceToPlayer = Vector3.Distance(_transform.position, _player.transform.position);
+            if (distanceToPlayer < detectionDistance && distanceToPlayer > closeEnoughDistance)
+            {
+                var vectorToPlayer = (_player.transform.position - _transform.position).normalized;
+                _transform.Translate(vectorToPlayer * speed * Time.deltaTime);
+            }
             _transform.LookAt(_player.transform);
         }
     }
