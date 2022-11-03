2a9ab481241bc8c38a56fe51e3d7a76909b87aa9
Andrew Allbright <aallbrig@gmail.com>
2022-11-03 13:04:59 -0400

    Try fix weirdness seen on mobile browser

--- unity/mecha-pilot/Assets/Scripts/Spawners/SpawnMods/PositionOnSphereCollider.cs
+++ unity/mecha-pilot/Assets/Scripts/Spawners/SpawnMods/PositionOnSphereCollider.cs
@@ -7,22 +7,19 @@ namespace Spawners.SpawnMods
         public SphereCollider sphereCollider;
         public float insetValue;
         private bool _active;
-        private Transform sphereColliderTransform;
+        private Transform _sphereColliderTransform;
         private void Start()
         {
             Spawner.Spawned += PositionSpawnedOnSphereCollider;
-            if (sphereCollider) sphereColliderTransform = sphereCollider.gameObject.transform;
+            if (sphereCollider) _sphereColliderTransform = sphereCollider.gameObject.transform;
         }
-        private void OnValidate() => _active = sphereCollider != default;
         private void PositionSpawnedOnSphereCollider(GameObject spawnedGameObject)
         {
-            if (!_active) return;
-
             var randomizedPosition = Random.insideUnitCircle.normalized * sphereCollider.radius;
             var directionToCenter = (randomizedPosition - Vector2.zero).normalized;
             var insetOffset = directionToCenter * insetValue;
             var calculatedPosition =
-                sphereColliderTransform.position + new Vector3(
+                _sphereColliderTransform.position + new Vector3(
                     randomizedPosition.x - insetOffset.x,
                     randomizedPosition.y - insetOffset.y,
                     0
