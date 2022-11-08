0a9a1f704cd7eb48da0005d2cd7b32666d410310
Andrew Allbright <aallbrig@gmail.com>
2022-11-07 21:07:42 -0500

    Support more control schemes

--- unity/mecha-pilot/Assets/Scripts/Character/PlayerController.cs
+++ unity/mecha-pilot/Assets/Scripts/Character/PlayerController.cs
@@ -3,18 +3,21 @@ using Combat;
 using Gameplay;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using PlayerInput = Generated.PlayerInput;
 
 namespace Character
 {
     [RequireComponent(typeof(CharacterController))]
     [RequireComponent(typeof(ObjectPool))]
-    public class PlayerController : MonoBehaviour, ICanDie
+    [RequireComponent(typeof(UnityEngine.InputSystem.PlayerInput))]
+    public class PlayerController : MonoBehaviour, ICanDie, IProcessMovement, IProcessFireWeaponCommand
     {
         public float speed = 7f;
         public GameObject bulletPrefab;
         public float timeBetweenShotsInSeconds = 0.25f;
         public ObjectPool bulletPool;
         private CharacterController _characterController;
+        private PlayerInput _playerInput;
         private float _timeLastFired;
         private Transform _transform;
 
@@ -25,6 +28,17 @@ namespace Character
         public Vector2 MoveInput { get; private set; }
 
         public Vector2 FireInput { get; private set; }
+
+        private void Awake()
+        {
+            _playerInput = new PlayerInput();
+            _playerInput.GamePlay.Move.started += HandleMovement;
+            _playerInput.GamePlay.Move.performed += HandleMovement;
+            _playerInput.GamePlay.Move.canceled += HandleMovement;
+            _playerInput.GamePlay.Fire.started += HandleFireWeaponCommand;
+            _playerInput.GamePlay.Fire.performed += HandleFireWeaponCommand;
+            _playerInput.GamePlay.Fire.canceled += HandleFireWeaponCommand;
+        }
 
         private void Start()
         {
@@ -40,34 +54,45 @@ namespace Character
         }
         private void Update()
         {
-            if (Gamepad.current == null) return;
-            MoveInput = Gamepad.current.leftStick.ReadValue();
-            FireInput = Gamepad.current.rightStick.ReadValue();
+            // if (Gamepad.current == null) return;
+            _characterController.Move(PlayerMoveVector * Time.deltaTime);
+            if (PlayerMoveVector != Vector3.zero)
+                _transform.rotation = Quaternion.LookRotation(new Vector3(MoveInput.x, MoveInput.y, 0));
+            // MoveInput = Gamepad.current.leftStick.ReadValue();
+            // FireInput = Gamepad.current.rightStick.ReadValue();
             // player is facing camera, so x is backwards -- correcting now
-            MoveInput = new Vector2(-MoveInput.x, MoveInput.y);
-            FireInput = new Vector2(-FireInput.x, FireInput.y);
-            PlayerMoveVector = new Vector3(MoveInput.x, MoveInput.y, 0) * speed;
+            // MoveInput = new Vector2(-MoveInput.x, MoveInput.y);
+            // FireInput = new Vector2(-FireInput.x, FireInput.y);
+            // PlayerMoveVector = new Vector3(MoveInput.x, MoveInput.y, 0) * speed;
+            // FireDirection = new Vector3(FireInput.x, FireInput.y, 0).normalized;
+            // Move();
             FireDirection = new Vector3(FireInput.x, FireInput.y, 0).normalized;
-            Move();
             if (FireInput != Vector2.zero && Time.time - _timeLastFired > timeBetweenShotsInSeconds) Fire();
         }
+        private void OnEnable() => _playerInput.Enable();
+        private void OnDisable() => _playerInput.Disable();
 
         private void OnCollisionEnter(Collision collision) => Died?.Invoke(gameObject);
 
         public event Action<GameObject> Died;
 
+        public void HandleFireWeaponCommand(InputAction.CallbackContext context)
+        {
+            FireInput = context.ReadValue<Vector2>();
+            FireInput = new Vector2(-FireInput.x, FireInput.y);
+        }
+
+        public void HandleMovement(InputAction.CallbackContext context)
+        {
+            MoveInput = context.ReadValue<Vector2>();
+            MoveInput = new Vector2(-MoveInput.x, MoveInput.y);
+            PlayerMoveVector = new Vector3(MoveInput.x, MoveInput.y, 0) * speed;
+        }
         private void Move()
         {
             _characterController.Move(PlayerMoveVector * Time.deltaTime);
             if (PlayerMoveVector != Vector3.zero)
                 _transform.rotation = Quaternion.LookRotation(new Vector3(MoveInput.x, MoveInput.y, 0));
-        }
-        private GameObject CreatePoolObject()
-        {
-            var bullet = Instantiate(bulletPrefab);
-            bullet.SetActive(false);
-            bullet.layer = gameObject.layer;
-            return bullet;
         }
         private void Fire()
         {
@@ -82,5 +107,15 @@ namespace Character
             _timeLastFired = Time.time;
         }
         private GameObject GetBulletFromPool() => bulletPool.GetPoolObject();
+    }
+
+    public interface IProcessMovement
+    {
+        public void HandleMovement(InputAction.CallbackContext context);
+    }
+
+    public interface IProcessFireWeaponCommand
+    {
+        public void HandleFireWeaponCommand(InputAction.CallbackContext context);
     }
 }
--- unity/mecha-pilot/Assets/Scripts/Gameplay/GameManager.cs
+++ unity/mecha-pilot/Assets/Scripts/Gameplay/GameManager.cs
@@ -13,21 +13,22 @@ namespace Gameplay
         GameOver
     }
 
-    public class GameManager : MonoBehaviour
+    public class GameManager : MonoBehaviour, IInitializePlay
     {
         public GameObject player;
         public float enoughTimeHasPassed = 2f;
         public Button playButton;
         public Button resetButton;
+        public GameState startingGameState = GameState.Menu;
         private GameState _currentState;
         private IFsm _fsm;
         private bool _gameOver;
         private float _gameOverTime;
-        private bool _playGameButton;
+        private bool _playerWantsToPlayGame;
         private bool _resetButtonClicked;
         private void Start()
         {
-            if (playButton) playButton.onClick.AddListener(() => _playGameButton = true);
+            if (playButton) playButton.onClick.AddListener(PlayGame);
             if (resetButton) resetButton.onClick.AddListener(() => _resetButtonClicked = true);
             if (player && player.TryGetComponent<ICanDie>(out var ableToDie))
                 ableToDie.Died += _ =>
@@ -36,74 +37,82 @@ namespace Gameplay
                     _gameOverTime = Time.time;
                 };
 
-            _fsm = new FsmBuilder()
-                .Owner(gameObject)
-                .Default(GameState.Menu)
-                .State(GameState.Menu, builder =>
-                {
-                    builder
-                        .Enter(_ =>
-                        {
-                            _playGameButton = false;
-                            GameManagerStateEntered?.Invoke(GameState.Menu);
-                        })
-                        .SetTransition(nameof(GameState.GamePlay), GameState.GamePlay)
-                        .Exit(_ =>
-                        {
-                            GameManagerStateExited?.Invoke(GameState.Menu);
-                        })
-                        .Update(action =>
-                        {
-                            if (_playGameButton) action.Transition(nameof(GameState.GamePlay));
-                        });
-
-                })
-                .State(GameState.GameOver, builder =>
-                {
-                    builder
-                        .Enter(_ =>
-                        {
-                            _resetButtonClicked = false;
-                            GameManagerStateEntered?.Invoke(GameState.GameOver);
-                        })
-                        .Exit(_ =>
-                        {
-                            GameManagerStateExited?.Invoke(GameState.GameOver);
-                            GameIsOver?.Invoke();
-                        })
-                        .SetTransition(nameof(GameState.Menu), GameState.Menu)
-                        .Update(action =>
-                        {
-                            if (_resetButtonClicked) action.Transition(nameof(GameState.Menu));
-                        });
-                })
-                .State(GameState.GamePlay, builder =>
-                {
-                    builder
-                        .Enter(_ =>
-                        {
-                            _gameOver = false;
-                            GameManagerStateEntered?.Invoke(GameState.GamePlay);
-                        })
-                        .Exit(_ =>
-                        {
-                            GameManagerStateExited?.Invoke(GameState.GamePlay);
-                        })
-                        .SetTransition(nameof(GameState.GameOver), GameState.GameOver)
-                        .Update(action =>
-                        {
-                            if (_gameOver && Time.time - _gameOverTime > enoughTimeHasPassed)
-                                action.Transition(nameof(GameState.GameOver));
-                        });
-                })
-                .Build();
+            _fsm = GetFiniteStateMachineDefinition().Build();
         }
         private void Update() => _fsm.Tick();
+        private void OnValidate() => _fsm = GetFiniteStateMachineDefinition().Build();
+
+        public void PlayGame() => _playerWantsToPlayGame = true;
+        private FsmBuilder GetFiniteStateMachineDefinition() => new FsmBuilder()
+            .Owner(gameObject)
+            .Default(startingGameState)
+            .State(GameState.Menu, builder =>
+            {
+                builder
+                    .Enter(_ =>
+                    {
+                        _playerWantsToPlayGame = false;
+                        GameManagerStateEntered?.Invoke(GameState.Menu);
+                    })
+                    .SetTransition(nameof(GameState.GamePlay), GameState.GamePlay)
+                    .Exit(_ =>
+                    {
+                        GameManagerStateExited?.Invoke(GameState.Menu);
+                    })
+                    .Update(action =>
+                    {
+                        if (_playerWantsToPlayGame) action.Transition(nameof(GameState.GamePlay));
+                    });
+
+            })
+            .State(GameState.GameOver, builder =>
+            {
+                builder
+                    .Enter(_ =>
+                    {
+                        _resetButtonClicked = false;
+                        GameManagerStateEntered?.Invoke(GameState.GameOver);
+                    })
+                    .Exit(_ =>
+                    {
+                        GameManagerStateExited?.Invoke(GameState.GameOver);
+                        GameIsOver?.Invoke();
+                    })
+                    .SetTransition(nameof(GameState.Menu), GameState.Menu)
+                    .Update(action =>
+                    {
+                        if (_resetButtonClicked) action.Transition(nameof(GameState.Menu));
+                    });
+            })
+            .State(GameState.GamePlay, builder =>
+            {
+                builder
+                    .Enter(_ =>
+                    {
+                        _gameOver = false;
+                        GameManagerStateEntered?.Invoke(GameState.GamePlay);
+                    })
+                    .Exit(_ =>
+                    {
+                        GameManagerStateExited?.Invoke(GameState.GamePlay);
+                    })
+                    .SetTransition(nameof(GameState.GameOver), GameState.GameOver)
+                    .Update(action =>
+                    {
+                        if (_gameOver && Time.time - _gameOverTime > enoughTimeHasPassed)
+                            action.Transition(nameof(GameState.GameOver));
+                    });
+            });
 
         public event Action<GameState> GameManagerStateEntered;
 
         public event Action<GameState> GameManagerStateExited;
 
         public event Action GameIsOver;
+    }
+
+    public interface IInitializePlay
+    {
+        public void PlayGame();
     }
 }
