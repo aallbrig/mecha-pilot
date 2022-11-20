153da2e51453ef511ab22cd51f89791c66f90e03
Andrew Allbright <aallbrig@gmail.com>
2022-11-20 10:13:20 -0500

    Refactor how to handle on death event

---
+++ unity/mecha-pilot/Assets/Scripts/Combat/Editor/OnDiedTriggerEditor.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Combat.Editor
+{
+    [CustomEditor(typeof(OnDiedTrigger))]
+    public class OnDiedTriggerEditor : UnityEditor.Editor
+    {
+        private OnDiedTrigger _componentInstance;
+        private void OnEnable() => _componentInstance = (OnDiedTrigger)target;
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Trigger Death Event"))
+                _componentInstance.HandleOnDied(_componentInstance.gameObjectAbleToDie);
+            base.OnInspectorGUI();
+        }
+    }
+}
+#endif
---
+++ unity/mecha-pilot/Assets/Scripts/Combat/OnDiedTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Combat
+{
+    public class OnDiedTrigger : MonoBehaviour
+    {
+        public GameObject gameObjectAbleToDie;
+        public UnityEvent onDied;
+        private ICanDie _ableToDie;
+        private void Awake() => Setup();
+
+        private void OnEnable() => _ableToDie.Died += HandleOnDied;
+        private void OnDisable() => _ableToDie.Died -= HandleOnDied;
+        private void OnValidate() => Setup();
+        public void HandleOnDied(GameObject deadGameObject) => onDied?.Invoke();
+        private void Setup()
+        {
+            if (!RegisterGameObjectAbleToDie())
+            {
+                Debug.LogError($"{name} unable to find something able to die");
+                enabled = false;
+            }
+        }
+        private bool RegisterGameObjectAbleToDie()
+        {
+            gameObjectAbleToDie ??= gameObject;
+            if (gameObjectAbleToDie.TryGetComponent<ICanDie>(out var ableToDie))
+            {
+                _ableToDie = ableToDie;
+                return true;
+            }
+            return false;
+        }
+    }
+}
