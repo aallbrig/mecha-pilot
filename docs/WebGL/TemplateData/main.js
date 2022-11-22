ccd9627e1d0ab77205a840967de4310a86e36796
aallbrig <aallbrig@gmail.com>
2022-11-22 18:41:14 +0000

    Deploying to github-pages from @ aallbrig/mecha-pilot@c8d93a5f1c173ee9276e1c2e2c1d25fb316392c9 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -1,13 +1,13 @@
 var canvas = document.querySelector("#unity-canvas");
 
 var config = {
-    dataUrl: "Build/WebGL.data.unityweb",
-    frameworkUrl: "Build/WebGL.framework.js.unityweb",
-    codeUrl: "Build/WebGL.wasm.unityweb",
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "Mecha Pilot",
-    productVersion: "0.0.59",
+    productVersion: "0.0.60",
     devicePixelRatio: 1,
 }
 
