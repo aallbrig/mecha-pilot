0b41c9a8a92cae2c77e53e1e3f2882a6957c0482
Andrew Allbright <aallbrig@gmail.com>
2022-11-26 22:03:40 -0500

    Indicators reset between game rounds

--- unity/mecha-pilot/Assets/Scripts/GamePlay/IndicatorManager.cs
+++ unity/mecha-pilot/Assets/Scripts/GamePlay/IndicatorManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Combat;
 using UnityEngine;
 using UnityEngine.Pool;
@@ -10,8 +12,27 @@ namespace Gameplay
         public GameObject indicatorPrefab;
         public Vector3 offset = Vector3.zero;
         public float distanceAway = 2f;
+        public GameManager gameManager;
+        private readonly List<GameObject> _activePoolItems = new();
         private ObjectPool<GameObject> _indicatorPool;
-        private void Start() => _indicatorPool = new ObjectPool<GameObject>(CreatePoolItem, OnGet, OnRelease);
+        public void Reset()
+        {
+            foreach (var indicator in _activePoolItems)
+                try
+                {
+                    _indicatorPool.Release(indicator);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            _activePoolItems.Clear();
+        }
+        private void Start()
+        {
+            _indicatorPool = new ObjectPool<GameObject>(CreatePoolItem, OnGet, OnRelease);
+            if (gameManager) gameManager.GameIsOver += Reset;
+        }
         private void OnRelease(GameObject poolItem) => poolItem.SetActive(false);
         private void OnGet(GameObject poolItem) => poolItem.SetActive(true);
         private GameObject CreatePoolItem()
@@ -28,6 +49,7 @@ namespace Gameplay
         public void IndicatorFor(GameObject track)
         {
             var poolItem = _indicatorPool.Get();
+            _activePoolItems.Add(poolItem);
             if (poolItem.TryGetComponent<Indicator>(out var indicator))
                 indicator.SetTrack(track.transform);
             if (track.TryGetComponent<ICanDie>(out var ableToDie))
@@ -36,6 +58,7 @@ namespace Gameplay
                 {
                     ableToDie.Died -= OnDied;
                     _indicatorPool.Release(poolItem);
+                    _activePoolItems.Remove(poolItem);
                 }
 
                 ableToDie.Died += OnDied;
