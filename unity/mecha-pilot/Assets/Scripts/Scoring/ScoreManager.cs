02c689343dc5df7c73c74bb616cae9a6b7ea3277
Andrew Allbright <aallbrig@gmail.com>
2022-12-02 15:00:36 -0500

    Simple save system

--- unity/mecha-pilot/Assets/Scripts/Scoring/ScoreManager.cs
+++ unity/mecha-pilot/Assets/Scripts/Scoring/ScoreManager.cs
@@ -6,13 +6,29 @@ using UnityEngine.Events;
 
 namespace Scoring
 {
+    [Serializable]
+    public class ScoreSave
+    {
+        public List<ScoreRecord> scores;
+        public ScoreSave(List<ScoreRecord> scores) => this.scores = scores;
+    }
+
     public class ScoreManager : MonoBehaviour
     {
         public InitialLetterInputForm form;
         public ScoreRecord currentScore;
         public List<ScoreRecord> scores;
         public UnityEvent<ScoreAddedResult> onScoreAdded;
+        private readonly string _saveKey = "MF_Scores";
 
+        private void Awake()
+        {
+            if (PlayerPrefs.HasKey(_saveKey))
+            {
+                var value = JsonUtility.FromJson<ScoreSave>(PlayerPrefs.GetString(_saveKey));
+                scores = value.scores;
+            }
+        }
         public void Reset()
         {
             currentScore = new ScoreRecord
@@ -49,6 +65,9 @@ namespace Scoring
         {
             if (form) currentScore.initials = form.GetInitials();
             scores.Add(currentScore);
+            var jsonString = JsonUtility.ToJson(new ScoreSave(scores));
+            PlayerPrefs.SetString(_saveKey, jsonString);
+            PlayerPrefs.Save();
         }
     }
 }
